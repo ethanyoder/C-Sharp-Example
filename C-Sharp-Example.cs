dad134fd26678fa5522045fb07e0ba0d4c50df11
Ethan Yoder <EMY3@pct.edu>
2017-10-31 08:43:08 -0400

    Add files via upload

---
+++ C-Sharp-Example.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter year born:");
+            int yearBorn = System.Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter age");
+            int yearsAlive = System.Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = yearBorn; i <= yearBorn + yearsAlive; i++)
+            {
+                Console.WriteLine(name + ": " + i);
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
